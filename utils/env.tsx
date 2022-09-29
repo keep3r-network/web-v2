87517bedccb88547a3557aee0dd6f7bf7945703f
Yunoek <Yunoek@pm.me>
2022-09-29 18:37:17 +0200

    lint

--- utils/env.tsx
+++ utils/env.tsx
@@ -1,3 +1,4 @@
+import {toAddress} from '@yearn-finance/web-lib/utils';
 import {ethers} from 'ethers';
 import {TEnv, TEnvData} from './types.d';
 
@@ -7,15 +8,15 @@ export function getEnv(chainID: number, canFallback = true): TEnvData {
 		return (process.env as TEnv).CHAINS[1];
 	} else if (!envForChain && !canFallback) {
 		return {
-			THE_KEEP3R: ethers.constants.AddressZero,
-			KEEP3R_V1_ADDR: ethers.constants.AddressZero,
-			KEEP3R_V2_ADDR: ethers.constants.AddressZero,
-			KP3R_TOKEN_ADDR: ethers.constants.AddressZero,
-			WETH_TOKEN_ADDR: ethers.constants.AddressZero,
-			KLP_KP3R_WETH_ADDR: ethers.constants.AddressZero,
-			UNI_KP3R_WETH_ADDR: ethers.constants.AddressZero,
-			CVX_TOKEN_ADDR: ethers.constants.AddressZero,
-			LIDO_TOKEN_ADDR: ethers.constants.AddressZero,
+			THE_KEEP3R: toAddress(ethers.constants.AddressZero),
+			KEEP3R_V1_ADDR: toAddress(ethers.constants.AddressZero),
+			KEEP3R_V2_ADDR: toAddress(ethers.constants.AddressZero),
+			KP3R_TOKEN_ADDR: toAddress(ethers.constants.AddressZero),
+			WETH_TOKEN_ADDR: toAddress(ethers.constants.AddressZero),
+			KLP_KP3R_WETH_ADDR: toAddress(ethers.constants.AddressZero),
+			UNI_KP3R_WETH_ADDR: toAddress(ethers.constants.AddressZero),
+			CVX_TOKEN_ADDR: toAddress(ethers.constants.AddressZero),
+			LIDO_TOKEN_ADDR: toAddress(ethers.constants.AddressZero),
 			EXPLORER: '',
 			BACKEND_URI: ''
 		};
