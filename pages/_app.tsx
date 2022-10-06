4aa3a8c992009566ba558f1d2adff9f49631bc13
Yunoek <Yunoek@pm.me>
2022-10-06 12:07:25 +0200

    5 supported chain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -274,7 +274,7 @@ function	MyApp(props: AppProps): ReactElement {
 				},
 				web3: {
 					defaultChainID: 1,
-					supportedChainID: [1, 10, 250, 1337]
+					supportedChainID: [1, 5, 10, 250, 1337]
 				}
 			}}>
 			<>
