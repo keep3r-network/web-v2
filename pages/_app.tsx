ebdef48ce26246c58558aece3cb3309747d8891a
Yunoek <Yunoek@pm.me>
2022-11-22 10:34:44 +0100

    fix: responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,9 @@ import {JobContextApp} from 'contexts/useJob';
 import Meta from 'components/Meta';
 import Footer from 'components/Footer';
 import LogoKeep3r from 'components/icons/Keep3r';
+import NetworkSelector from 'components/NetworkSelector';
 
 import	'../style.css';
-import NetworkSelector from 'components/NetworkSelector';
 
 /* 📰 - Keep3r *****************************************************************
 ** Little hack in order to get the correct context based on the page. In short,
@@ -131,6 +131,12 @@ function	AppWithLayout(props: AppProps): ReactElement {
 								<div />
 							</div>
 						</Link>
+						<Link href={'/debt'}>
+							<div aria-selected={pathname === '/debt'} className={'menu_item px-5'}>
+								<b>{'Debt'}</b>
+								<div />
+							</div>
+						</Link>
 						<Link href={'/disputes'}>
 							<div aria-selected={pathname === '/disputes'} className={'menu_item px-5'}>
 								<b>{'Disputes'}</b>
@@ -212,6 +218,19 @@ function	AppWithLayout(props: AppProps): ReactElement {
 							<div className={'flex flex-row items-center space-x-3'}>
 								<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 512 512'} className={'h-4 w-4'}><path d={'M256 64C397.4 64 512 128.5 512 208C512 287.5 397.4 352 256 352C114.6 352 0 287.5 0 208C0 128.5 114.6 64 256 64zM0 290.1C13.21 305.8 29.72 319.5 48 330.1V394.6C17.79 373.6 0 347.9 0 320V290.1zM80 412.1V348.3C108.4 361.4 140.9 371.3 176 377.3V441.6C139.8 435.7 107.1 425.8 80 412.1zM208 381.6C223.7 383.2 239.7 384 256 384C272.3 384 288.3 383.2 304 381.6V445.8C288.5 447.2 272.4 448 256 448C239.6 448 223.5 447.2 208 445.8V381.6zM336 441.6V377.3C371.1 371.3 403.6 361.4 432 348.3V412.1C404.9 425.8 372.2 435.7 336 441.6zM464 330.1C482.3 319.5 498.8 305.8 512 290.1V320C512 347.9 494.2 373.6 464 394.6V330.1z'} fill={'currentcolor'} /></svg>
 								<b>{'Treasury'}</b>
+							</div>
+							<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 512 512'} className={'h-4 w-4 text-grey-2/50'}><path d={'M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM358.6 278.6l-112 112c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25L290.8 256L201.4 166.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l112 112C364.9 239.6 368 247.8 368 256S364.9 272.4 358.6 278.6z'} fill={'currentcolor'} /></svg>
+						</div>
+					</Link>
+
+					<Link href={'/debt'} key={'/debt'}>
+						<div
+							onClick={(): void => set_hasMobileMenu(false)}
+							aria-selected={pathname === '/debt'}
+							className={'flex flex-row items-center justify-between bg-grey-4 px-4 py-3 text-base font-bold'}>
+							<div className={'flex flex-row items-center space-x-3'}>
+								<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 384 512'} className={'h-4 w-4'}><path d={'M14 2.2C22.5-1.7 32.5-.3 39.6 5.8L80 40.4 120.4 5.8c9-7.7 22.3-7.7 31.2 0L192 40.4 232.4 5.8c9-7.7 22.2-7.7 31.2 0L304 40.4 344.4 5.8c7.1-6.1 17.1-7.5 25.6-3.6s14 12.4 14 21.8V488c0 9.4-5.5 17.9-14 21.8s-18.5 2.5-25.6-3.6L304 471.6l-40.4 34.6c-9 7.7-22.2 7.7-31.2 0L192 471.6l-40.4 34.6c-9 7.7-22.3 7.7-31.2 0L80 471.6 39.6 506.2c-7.1 6.1-17.1 7.5-25.6 3.6S0 497.4 0 488V24C0 14.6 5.5 6.1 14 2.2zM96 144c-8.8 0-16 7.2-16 16s7.2 16 16 16H288c8.8 0 16-7.2 16-16s-7.2-16-16-16H96zM80 352c0 8.8 7.2 16 16 16H288c8.8 0 16-7.2 16-16s-7.2-16-16-16H96c-8.8 0-16 7.2-16 16zM96 240c-8.8 0-16 7.2-16 16s7.2 16 16 16H288c8.8 0 16-7.2 16-16s-7.2-16-16-16H96z'}/></svg>
+								<b>{'Debt'}</b>
 							</div>
 							<svg xmlns={'http://www.w3.org/2000/svg'} viewBox={'0 0 512 512'} className={'h-4 w-4 text-grey-2/50'}><path d={'M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM358.6 278.6l-112 112c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25L290.8 256L201.4 166.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l112 112C364.9 239.6 368 247.8 368 256S364.9 272.4 358.6 278.6z'} fill={'currentcolor'} /></svg>
 						</div>
--- pages/debt.tsx
+++ pages/debt.tsx
@@ -1,5 +1,5 @@
 import React, {ReactElement, useEffect, useState} from 'react';
-import {format} from '@yearn-finance/web-lib/utils';
+import {format, truncateHex} from '@yearn-finance/web-lib/utils';
 import {useDebt} from 'contexts/useDebt';
 import useSWR from 'swr';
 import axios from 'axios';
@@ -23,9 +23,11 @@ function	Debt(): ReactElement {
 				<div className={'flex items-center justify-center py-6 px-4 md:px-0'}>
 					<div className={'space-y-2 text-center'}>
 						<p>{'Debt'}</p>
-						<div><b className={'text-2xl tabular-nums'}>
-							{`$ ${tvlUSD == 0 ? '0.00' : format.amount(tvlUSD, 2, 2)}`}
-						</b></div>
+						<div>
+							<b className={'text-2xl tabular-nums'}>
+								{`$ ${tvlUSD == 0 ? '0.00' : format.amount(tvlUSD, 2, 2)}`}
+							</b>
+						</div>
 					</div>
 				</div> 
 			</section>
@@ -37,30 +39,48 @@ function	Debt(): ReactElement {
 							.map((debt): ReactElement => {
 								const	data = ibAPIData?.find((ib: any): boolean => ib.underlying_symbol.toLowerCase() === debt.name.toLowerCase());
 								return (
-									<div key={debt.name} className={'bg-white pt-6 pl-6 pr-10 pb-8'}>
+									<div key={debt.name} className={'bg-white p-6 md:pt-6 md:pl-6 md:pr-10 md:pb-8'}>
 										<p>{debt.name}</p>
-										<div className={'mt-2 flex flex-col justify-center space-y-1'}>
-											<h3 className={'text-3xl font-bold tabular-nums'}>
-												{format.amount(format.toNormalizedValue(debt.totalBorrowBalance, 18), 2, 2)}
-											</h3>
-											<p>
-												{`$ ${format.amount(debt.totalBorrowValue, 2, 2)}`}
-											</p>
+										<div className={'mt-2 flex flex-row items-center space-x-10 md:space-x-10'}>
+											<div className={'flex flex-col justify-center space-y-1'}>
+												<h3 className={'text-2xl font-bold tabular-nums md:text-3xl'}>
+													{format.amount(format.toNormalizedValue(debt.totalBorrowBalance, 18), 2, 2)}
+												</h3>
+												<p className={'text-sm md:text-base'}>
+													{`$ ${format.amount(debt.totalBorrowValue, 2, 2)}`}
+												</p>
+											</div>
+											<div className={'flex flex-col justify-center space-y-1'}>
+												<h3 className={'text-2xl font-bold tabular-nums md:text-3xl'}>
+													{`${format.amount((data?.borrow_apy?.value * 100) || 0, 2, 2)} %`}
+												</h3>
+												<p className={'text-sm md:text-base'}>
+													{'Borrow APY'}
+												</p>
+											</div>
 										</div>
 										<div className={'mt-6 grid grid-cols-1 gap-6'}>
-											<div className={'grid grid-cols-5'}>
-												<b className={'col-span-2'}>{'Borrower'}</b>
-												<b className={'text-right'}>{'Debt, ibCHF'}</b>
+											<div className={'hidden grid-cols-5 md:grid'}>
+												<b className={'col-span-3'}>{'Borrower'}</b>
+												<b className={'text-right'}>{`Debt, ${debt.name}`}</b>
 												<b className={'text-right'}>{'Debt, $'}</b>
-												<b className={'text-right'}>{'Borrow APY'}</b>
 											</div>
 
-											{Object.entries(debt.borrowBalance).map(([borrower, balance], index): ReactElement => (
-												<div key={borrower} className={'grid grid-cols-5'}>
-													<p className={'col-span-2 underline'}>{borrower}</p>
-													<p className={'text-right'}>{format.amount(balance.normalized, 2, 2)}</p>
-													<p className={'text-right'}>{format.amount(balance.normalizedValue, 2, 2)}</p>
-													<p className={'text-right'}>{index === 0 ? `${format.amount((data?.borrow_apy?.value * 100) || 0, 2, 2)} %` : ''}</p>
+											{Object.entries(debt.borrowBalance).map(([borrower, balance]): ReactElement => (
+												<div key={borrower} className={'grid grid-cols-1 md:grid-cols-5'}>
+													<p className={'col-span-3 hidden underline md:block'}>{borrower}</p>
+													<p className={'col-span-1 flex flex-row justify-between md:hidden'}>
+														<span className={'block text-xs text-neutral-400 md:hidden'}>{'Borrower'}</span>
+														{truncateHex(borrower, 6)}
+													</p>
+													<p className={'flex flex-row justify-between text-left md:block md:text-right'}>
+														<span className={'block text-xs text-neutral-400 md:hidden'}>{`Debt, ${debt.name}`}</span>
+														{format.amount(balance.normalized, 2, 2)}
+													</p>
+													<p className={'flex flex-row justify-between text-left md:block md:text-right'}>
+														<span className={'block text-xs text-neutral-400 md:hidden'}>{'Debt, $'}</span>
+														{format.amount(balance.normalizedValue, 2, 2)}
+													</p>
 												</div>
 											))}
 										</div>
