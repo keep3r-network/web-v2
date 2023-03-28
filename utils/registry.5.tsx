95bfc462a403025863e2cc4f5b35e948616066eb
Rahul Sethuram <rahul@sethuram.com>
2023-03-28 08:56:44 +0400

    Update registry

--- utils/registry.5.tsx
+++ utils/registry.5.tsx
@@ -6,6 +6,18 @@ const	REGISTRY: TRegistry = {
 		address: '0xa2c7A15FFc02e00cdeedBba56c41dAaed84f8734',
 		name: 'BasicJob',
 		repository: 'https://github.com/keep3r-network/keep3r-network-v2'
+	},
+	'0x81Bf17fB3b129dE1Fd3C58D6A1785061d763B863': {
+		chainID: 5,
+		address: '0x81Bf17fB3b129dE1Fd3C58D6A1785061d763B863',
+		name: 'ConnextRelayerProxy',
+		repository: 'https://github.com/connext/connext-keeper-scripts'
+	},
+	'0x8AcA83F46d57a1d2C7E0a72E44687c75aF6cbe21': {
+		chainID: 5,
+		address: '0x8AcA83F46d57a1d2C7E0a72E44687c75aF6cbe21',
+		name: 'ConnextRelayerProxy',
+		repository: 'https://github.com/connext/connext-keeper-scripts'
 	}
 };
 
