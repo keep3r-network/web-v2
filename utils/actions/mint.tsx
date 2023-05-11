e9f04d9abdedb9bc4f7252f038e2ba640d92b570
Yunoek <89300671+Yunoek@users.noreply.github.com>
2023-05-11 20:20:34 +0200

    Update mint.tsx

--- utils/actions/mint.tsx
+++ utils/actions/mint.tsx
@@ -24,8 +24,8 @@ export async function	mint(
 	return await handleTx(contract.mint(
 		amountToken1, //amount0Desired
 		amountToken2, //amount1Desired
-		ethers.utils.parseUnits((amountToken1String - (amountToken1String * 0.99)).toFixed(18), 18),
-		ethers.utils.parseUnits((amountToken2String - (amountToken2String * 0.99)).toFixed(18), 18),
+		ethers.utils.parseUnits((amountToken1String * 0.98).toFixed(18), 18),
+		ethers.utils.parseUnits((amountToken2String * 0.98).toFixed(18), 18),
 		address //to
 	));
 }
